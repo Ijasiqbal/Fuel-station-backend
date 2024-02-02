4724825f39d01a959a152c1d5ead8a1603482498
Ijas Iqbal <ijasiqbal4@gmail.com>
2024-02-03 03:48:23 +0530

    after flush

--- LoginApp/views.py
+++ LoginApp/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from .serializers import UserSerializer
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 
 
 # view for registering users
