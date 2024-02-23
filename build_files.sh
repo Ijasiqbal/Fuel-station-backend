55ec36ae5d68c6bf3780bdd8d1ef96bf08e04524
Ijas Iqbal <ijasiqbal4@gmail.com>
2024-02-23 15:55:45 +0530

    vercel connection code altered

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 pip install -r requirements.txt
 
 python manage.py makemigrations  --noinput
 python manage.py migrate  --noinput
 
-python manage.py collectstatic
+python manage.py collectstatic --noinput
