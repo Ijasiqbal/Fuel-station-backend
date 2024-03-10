738c8de4492cffb528730c82a9d8334b85ba715d
Ijas Iqbal <ijasiqbal4@gmail.com>
2024-03-10 12:28:29 +0530

    db updated

--- LoginApp/views.py
+++ LoginApp/views.py
@@ -3,10 +3,12 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from .serializers import UserSerializer
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 
 
 # view for registering users
 class RegisterView(APIView):
+    #permission_classes = [AllowAny]
     def post(self, request):
         serializer = UserSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
